ea97e5f8bb961f96dcfd2220476c59f668f8ca5a
d3vil <davidchen1011@gmail.com>
2020-04-10 22:16:16 +0800

    '清除bug'

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -7,7 +7,15 @@
         <Loading v-if="isLoading"></Loading>
         <div id="content" class="contentDetail" v-else>
             <div class="detail_list">
-                <div class="detail_list_bg"></div>
+                <div
+                    class="detail_list_bg"
+                    :style="{
+                        'background-image':
+                            'url(' +
+                            detailMovie.img.replace(/w\.h/, '148.208') +
+                            ')'
+                    }"
+                ></div>
                 <div class="detail_list_filter"></div>
                 <div class="detail_list_content">
                     <div class="detail_list_img">
@@ -140,7 +148,7 @@ export default {
 .detail_list .detail_list_bg {
     width: 100%;
     height: 100%;
-    background: url(/images/movie_1.jpg) 0 40%;
+    background: 0 40%;
     filter: blur(20px);
     background-size: cover;
     position: absolute;
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	devServer: {
-		open: true,
-		host: 'localhost',
-		port: 8080,
-		https: false,
+		// open: true,
+		// host: 'localhost',
+		// port: 8080,
+		// https: false,
 		//以上的ip和端口是我们本机的;下面为需要跨域的
 		proxy: { //配置跨域
 			'/api': {
